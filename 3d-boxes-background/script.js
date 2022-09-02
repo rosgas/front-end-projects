7b2225d01785df6f846f511aed08ff109624f52c
rosgas <rgasbarri.274@gmail.com>
2022-09-02 11:53:33 +1000

    Updated

---
+++ 3d-boxes-background/script.js
@@ -0,0 +1,48 @@
+const button = document.querySelector(".btn");
+const boxes = document.getElementById("boxes");
+
+function changeBoxesSize(x) {
+  if (x.matches) {
+    createSmallBoxes();
+  } else {
+    createBigBoxes();
+  }
+}
+
+let x = window.matchMedia("(max-width: 700px)");
+changeBoxesSize(x);
+
+function createBigBoxes() {
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 4; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      console.log(box);
+      boxes.appendChild(box);
+    }
+  }
+}
+
+function createSmallBoxes() {
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 4; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.backgroundPosition = `${-j * 50}px ${-i * 50}px`;
+      boxes.appendChild(box);
+    }
+  }
+}
+
+button.addEventListener("click", () => {
+  button.classList.add("show");
+  setTimeout(() => button.classList.remove("show"), 500);
+  setTimeout(() => boxes.classList.add("effect"), 300);
+  setTimeout(() => boxes.classList.remove("effect"), 500);
+  setTimeout(() => boxes.classList.toggle("big"), 600);
+});
+
+window.onresize = function () {
+  location.reload();
+};
